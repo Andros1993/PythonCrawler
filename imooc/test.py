e2db51394d51f5eb17d128c8760c1ac7695226f5
Administrator <william@1stellar.com>
2017-08-09 14:02:22 +0800

    add header

--- imooc/test.py
+++ imooc/test.py
@@ -1,7 +1,9 @@
 # -*-coding:utf-8 -*-
 from urllib import request
 
-resp = request.urlopen("http://www.baidu.com")
-resp.add_header()
+req = request.Request("https://www.amazon.ca/s/ref=nb_sb_noss_1/137-1949719-4324128?url=search-alias%3Daps&field-keywords=iphone")
+req.add_header("User-Agent","Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36")
+
+resp = request.urlopen(req)
 
 print(resp.read().decode("utf-8"))
