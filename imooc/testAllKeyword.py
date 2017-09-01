fdb2665eb6475048f2bf85a2cd52e6faceaaafbb
Administrator <william@1stellar.com>
2017-09-01 14:03:16 +0800

    准备测试读取文件关键字

--- imooc/testAllKeyword.py
+++ imooc/testAllKeyword.py
@@ -3,6 +3,7 @@ from urllib import request
 import time
 import _thread
 from bs4 import BeautifulSoup as bs
+import re
 
 #           "fm transmitter",
 #     		"car bluetooth fm transmitter",
@@ -35,27 +36,33 @@ def init_url(url):
 
 def searchKeyworld(nextUrl, page):
     htmlContent = init_url(nextUrl)
+
     soup = bs(htmlContent, "html.parser")
-    liList = soup.find_all('li')
-    for text in soup.find_all('li'):
+    # li_list = soup.find_all('li', attrs={'class': 's-result-item  celwidget '})
+    li_list = soup.find_all('li', attrs={'id':re.compile('result_')})
+    for text in li_list:
         asin = text.get('data-asin')
-        if asin == 'B072KCY51R':
-            position = liList.index(text)
+        if asin == 'B073XLR5HW':
+            position = li_list.index(text) + 1
             if position % 3 == 0:
-                row_position = position / 3
+                row_position = int(position / 3)
                 list_position = 3
             else:
-                row_position = (position / 3) + 1
+                row_position = int(position / 3) + 1
                 list_position = position % 3
+            if str(text).find("a-spacing-none a-color-tertiary a-text-normal") == -1:
+                print("搜索排名在第" + str(page) + "页" + "，第" + str(row_position) + "行" + ",第" + str(list_position) + "个")
+            else:
+                print("广告在第" + str(page) + "页" + "，第" + str(row_position) + "行" + ",第" + str(list_position) + "个")
 
-            print("找到了，在第" + str(page) + "页，" + "，第" + str(row_position) + "行" + ",第" + str(list_position) + "个")
-            print(position)
+base_url = "https://www.amazon.ca/s/ref=nb_sb_noss?url=search-alias%3Delectronics&field-keywords=" + "fm+transmitter" + "&rh=n%3A667823011%2Ck%3A" + "fm+transmitter"
+htmlContent = init_url(base_url)
+# getResult = htmlContent.find("Bluetooth FM Transmitter,[Newest Version]Etybetopstar T11 Car Transmitter Radio Adapter Car Kit with 4 Music Play Mode/Hands-Free Calling/1.44 Inch Screen Display/USB Car")
 
-htmlContent = init_url("https://www.amazon.ca/s/ref=nb_sb_noss?url=search-alias%3Delectronics&field-keywords=" + "fm+transmitter" + "&rh=n%3A667823011%2Ck%3A" + "fm+transmitter")
-getResult = htmlContent.find("Perbeat BT66 Wireless Bluetooth FM Transmitter Hands free Car Kit Radio Adapter MP3 Player Dual USB Car Charger support Micro SD Card USB Flash Disk for Smart phone, iPhone, iPad,etc (Blue)")#"Bluetooth FM Transmitter,[Newest Version]Etybetopstar T11 Car Transmitter Radio Adapter Car Kit with 4 Music Play Mode/Hands-Free Calling/1.44 Inch Screen Display/USB Car Charger/Support TF Card/U Disk/AUX Input for Mobile Audio Devices,Black")
+# if getResult != -1:
+#     print("找到了，在第1页")
 
-if getResult != -1:
-    print("找到了，在第1页")
+_thread.start_new_thread(searchKeyworld, (base_url, 1,))
 
 index_start_string = htmlContent.find("/s/ref=sr_pg_2/")
 index_start = int(index_start_string)
@@ -67,7 +74,7 @@ pg2_undecode_url = pg2_undecode_url.replace(pg2_undecode_url[14 : 34],"")
 for i in range(5):
     pg2_undecode_url = pg2_undecode_url.replace("&amp;","&")
 
-for i in range(3,50):
+for i in range(2,10):
 
     nextUrl = "https://www.amazon.ca" + pg2_undecode_url.replace("/s/ref=sr_pg_2", "/s/ref=sr_pg_" + str(i))
     nextUrl = nextUrl.replace("page=2", "page=" + str(i))
@@ -75,4 +82,4 @@ for i in range(3,50):
         _thread.start_new_thread(searchKeyworld, (nextUrl, i,))
     except:
         print("Error: 无法启动线程")
-    time.sleep(0.8)
+    time.sleep(1)
