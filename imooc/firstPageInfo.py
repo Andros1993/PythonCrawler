1feecf295a44cc22d4c033cf45e7cafa0e75cea0
Administrator <william@1stellar.com>
2017-09-05 13:59:12 +0800

    删除广告位有问题，无法删除第2个和最后一个广告位

--- imooc/firstPageInfo.py
+++ imooc/firstPageInfo.py
@@ -8,9 +8,9 @@ import re
 def get_rul_conten(url):
     req = request.Request(url)
     req.add_header("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko")
-    req.add_header("Host", "www.amazon.ca")
+    req.add_header("Host", "www.amazon.com")
     req.add_header("Accept-Language", "zh-CN,zh;q=0.8,en-US;q=0.6,en;q=0.4")
-    req.add_header("Host", "www.amazon.ca")
+    req.add_header("Host", "www.amazon.com")
     req.add_header("Upgrade-Insecure-Requests", "1")
     req.add_header("Connection", "keep-alive")
     req.add_header("Cache-Control", "max-age=0")
@@ -21,7 +21,15 @@ def get_rul_conten(url):
 
 def get_first_page_all_list(htmlContent):
     soup = bs(htmlContent, "html.parser")
-
+    li_list = soup.find_all('li', attrs={'id': re.compile('result_')})
+    #删除所有广告链接
+    for lin_text in li_list:
+        if lin_text.find("a-spacing-none a-color-tertiary s-sponsored-list-header a-text-normal") != -1:
+            li_list.remove(lin_text)
+    #处理所有有效的链接
+    for lin_text in li_list:
+        print(lin_text)
 
 base_url = "https://www.amazon.com/s/ref=nb_sb_noss_1?url=search-alias%3Daps&field-keywords=knee+brace"
 htmlContent = get_rul_conten(base_url)
+get_first_page_all_list(htmlContent)
